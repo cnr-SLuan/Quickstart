c4936a1de18635f4b93888224a318028f1e39690
dingus4882 <dingus4882@gmail.com>
2025-10-31 09:56:47 -0700

    Create frijoles.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/frijoles.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@TeleOp
+public class frijoles extends OpMode{
+    @Override
+    public void init(){
+        telemetry.addData("Status", "Running");
+    }
+    @Override
+    public void loop(){
+
+    }
+}
