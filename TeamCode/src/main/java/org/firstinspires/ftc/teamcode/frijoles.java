733cd258e8fb75ca123ccfd8a146de2fbea0455f
dingus4882 <dingus4882@gmail.com>
2025-10-31 10:00:38 -0700

    Update frijoles.java
    Basic beginners program reference

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/frijoles.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/frijoles.java
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * This program will post "Status:Running" on the telemetry of the driver hub.
+ * */
 @TeleOp
 public class frijoles extends OpMode{
     @Override
