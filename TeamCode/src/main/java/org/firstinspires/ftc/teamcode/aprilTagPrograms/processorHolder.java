5bf7107f22cd16d831f403a5cdda769e1c5bda21
dingus4882 <dingus4882@gmail.com>
2026-01-16 16:59:11 -0800

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aprilTagPrograms/processorHolder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aprilTagPrograms/processorHolder.java
@@ -1,34 +1,24 @@
 package org.firstinspires.ftc.teamcode.aprilTagPrograms;
 
-//limelight
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.IMU;
 
-//lists
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Collections;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
-@TeleOp(name = "processor")
-public class processor extends OpMode {
+@TeleOp(name = "processorHolder")
+public class processorHolder extends OpMode {
     private Limelight3A limelight;
     private IMU imu;
-    LLResult llResult;
-
-    double min;
-    double max;
-    int temp;
-
-    ArrayList<ArrayList<Double>> myStats = new ArrayList<>();
-    ArrayList<Double> miniStats = new ArrayList<Double>();
+    private LLResultTypes.BarcodeResult BarcodeResult;
 
     @Override
     public void init() {
@@ -49,12 +39,13 @@ public class processor extends OpMode {
     public void loop() {
         YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
         limelight.updateRobotOrientation(orientation.getYaw());
-        llResult = limelight.getLatestResult();
+        LLResult llResult = limelight.getLatestResult();
         if (llResult != null && llResult.isValid()) {
             Pose3D botPose = llResult.getBotpose_MT2();
             telemetry.addData("Tx", llResult.getTx());
             telemetry.addData("Ty", llResult.getTy());
             telemetry.addData("Ta", llResult.getTa());
+            telemetry.addData("Distance", llResult.getBotposeAvgDist());
             List<LLResultTypes.FiducialResult> fiducials = llResult.getFiducialResults();
             if (fiducials.isEmpty()) {
                 telemetry.addLine("No AprilTags");
@@ -64,17 +55,6 @@ public class processor extends OpMode {
                     telemetry.addData("Tag ID", tag.getFiducialId());
                     if (tag.getFiducialId() == 22){
                         telemetry.addData("Current Team", "Blue");
-                        for (int i = 0; i < temp; i++){
-                            if (miniStats.get(i) == tag.getFiducialId()){
-                                i = temp;
-                                telemetry.addData("Shooting Status: ", "Safe to shoot");
-                            }
-                            else{
-                                telemetry.addData("Shooting Status: ", "Not safe to shoot");
-                                i++;
-                            }
-
-                        }
                     }
                     if (tag.getFiducialId() == 21){
                         telemetry.addData("Current Team", "Red");
@@ -87,29 +67,6 @@ public class processor extends OpMode {
             }
 
         }
-    }
-    private void redTeam(){
-        if (gamepad1.dpadUpWasPressed()){
-            miniStats.add(llResult.getTx());
-            miniStats.add(llResult.getTy());
-            miniStats.add(llResult.getTa());
-            myStats.add(miniStats);
-            while (!miniStats.isEmpty()){
-                Collections.sort(miniStats);
-                min = miniStats.get(0);
-                temp = miniStats.size();
-                temp =- 1;
-                max = miniStats.get(temp);
-            }
-        }
-        else{
-            telemetry.addLine("Nothing for now.");
-        }
-
-    }
-
-    private void shoot(){
-
     }
 
 }
